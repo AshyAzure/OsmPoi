542b952c0ea066e55511dfd9d0ef643e2b2f3661
Xubai Wang <kindredwekingwang@gmail.com>
2021-01-26 08:42:34 +0800

    Add ffi-convert back

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,43 +1,14 @@
 #![allow(non_camel_case_types)]
 
 use super::poi::OsmCount;
-use libc::c_ulong;
-use std::convert::From;
+use ffi_convert::{AsRust, CDrop, CReprOf};
 
 /// The C representation type of OsmCount.
 #[repr(C)]
+#[derive(CReprOf, AsRust, CDrop)]
+#[target_type(OsmCount)]
 pub struct OSMPOI_OsmCount {
-    pub node: c_ulong,
-    pub way: c_ulong,
-    pub relation: c_ulong,
-}
-
-impl From<OsmCount> for OSMPOI_OsmCount {
-    fn from(item: OsmCount) -> Self {
-        let OsmCount {
-            node,
-            way,
-            relation,
-        } = item;
-        OSMPOI_OsmCount {
-            node,
-            way,
-            relation,
-        }
-    }
-}
-
-impl Into<OsmCount> for OSMPOI_OsmCount {
-    fn into(self) -> OsmCount {
-        let Self {
-            node,
-            way,
-            relation,
-        } = self;
-        OsmCount {
-            node,
-            way,
-            relation,
-        }
-    }
+    pub node: u64,
+    pub way: u64,
+    pub relation: u64,
 }
