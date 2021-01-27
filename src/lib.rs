81db44478e53094ddbf4401513f710501b26e827
ubuntu <ubuntu@localhost.localdomain>
2021-01-28 04:19:29 +0800

    feat: add cli

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,57 @@
 pub mod csv;
-pub mod ffi;
 pub mod poi;
 
-pub use ffi::*;
+use anyhow::{Context, Result};
+use osmpbfreader::OsmPbfReader;
+use poi::{cal_relations, cal_ways, refine, OsmPbfReaderExt};
+use rusqlite::{Connection, OpenFlags};
+use std::path::{Path, PathBuf};
 
-fn dis_to_deg(dis: f32) -> i32 {
+pub fn data_dir() -> Result<PathBuf> {
+    // create data_dirs
+    let mut path = dirs::data_dir().context("Cannot open application support folder")?;
+    path.push("osmpoi");
+    Ok(path)
+}
+
+pub fn add_osm_pbf<P: AsRef<Path>>(pbf_path: P) -> Result<()> {
+    let pbf_path = pbf_path.as_ref();
+    let stem = pbf_path.file_stem().context("Cannot get file stem")?;
+    let db_path = data_dir()?.join(stem).with_extension("osm.poi");
+    let r = std::fs::File::open(pbf_path)?;
+    let mut pbf = OsmPbfReader::new(r);
+    let mut conn = Connection::open_with_flags(
+        db_path,
+        OpenFlags::SQLITE_OPEN_CREATE | OpenFlags::SQLITE_OPEN_READ_WRITE,
+    )?;
+    let mut tx = conn.transaction()?;
+    pbf.dump(&mut tx)?;
+    println!("dump success");
+    cal_ways(&mut tx)?;
+    println!("calculate way success");
+    cal_relations(&mut tx)?;
+    println!("calculate relation success");
+    refine(&mut tx)?;
+    println!("refine success");
+    println!("Finish!");
+    tx.commit()?;
+    Ok(())
+}
+
+pub fn dis_to_deg(dis: f32) -> i32 {
     (dis / 6_371_000. / std::f32::consts::PI * 180.) as i32
+}
+
+pub fn list_data_dir() -> Result<Vec<String>> {
+    let entries = std::fs::read_dir(data_dir()?.as_path())?;
+    let mut ret = Vec::new();
+    for entry in entries {
+        ret.push(
+            entry?
+                .file_name()
+                .into_string()
+                .map_err(|_| anyhow::Error::msg("Cannor get entry"))?,
+        );
+    }
+    Ok(ret)
 }
---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use ::osmpoi::*;
+use anyhow::{Context, Result};
+use clap::Clap;
+
+/// The cli version of osmpoi, which extract poi information from openstreetmap files.
+#[derive(Clap)]
+#[clap(version = "0.0.1", author = "Xubai Wang <kindredwekingwang@gmail.com>")]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    Ls(List),
+    Add(Add),
+    Export(Export),
+    Rm(Remove),
+}
+
+/// list all datasets
+#[derive(Clap)]
+struct List;
+
+/// add new dataset, only .osm, .osm.pbf, .osm.poi file formats are acceptable
+#[derive(Clap)]
+struct Add {
+    /// the file to add
+    path: String,
+}
+
+/// export dataset in osm.poi format
+#[derive(Clap)]
+struct Export {
+    /// the dataset to export
+    name: String,
+    /// where to export
+    path: String,
+}
+
+/// remove dataset
+#[derive(Clap)]
+struct Remove {
+    /// the file to remove
+    name: String,
+}
+
+fn main() -> Result<()> {
+    let opts: Opts = Opts::parse();
+    match opts.subcmd {
+        SubCommand::Ls(_list) => {
+            let items = list_data_dir()?;
+            for item in items {
+                println!("{}", item);
+            }
+        }
+        SubCommand::Add(add) => {
+            let path = data_dir()?;
+            std::fs::create_dir_all(path).context("Cannot create subdir")?;
+            add_osm_pbf(add.path)?;
+        }
+        SubCommand::Export(export) => {
+            let path = data_dir()?.join(export.name);
+            std::fs::copy(path, export.path)?;
+        }
+        SubCommand::Rm(remove) => {
+            let path = data_dir()?.join(remove.name);
+            std::fs::remove_file(path)?;
+        }
+    }
+
+    Ok(())
+}
