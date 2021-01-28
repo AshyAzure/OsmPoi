044505436f301f773acab44484a80c2d5c2cc67c
Xubai Wang <kindredwekingwang@gmail.com>
2021-01-28 22:48:08 +0800

    feat ui: add macOS native ui

---
+++ ui/OsmPoi for MacOS/OsmPoi for MacOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OsmPoi for MacOS
+//
+//  Created by 王维境 on 2021/1/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ui/OsmPoi for MacOS/OsmPoi for MacOS/OsmPoi_for_MacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OsmPoi_for_MacOSApp.swift
+//  OsmPoi for MacOS
+//
+//  Created by 王维境 on 2021/1/28.
+//
+
+import SwiftUI
+
+@main
+struct OsmPoi_for_MacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
